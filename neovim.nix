86fa6e6be31b9d5ed5a8991555a4fce2a422eef7
JaniAnttonen <jani.anttonen@protonmail.ch>
2022-06-10 13:21:22 +0300

    Add cairo.vim

--- neovim.nix
+++ neovim.nix
@@ -77,6 +77,16 @@
     ];
 
     extraConfig = ''
+        let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+        if empty(glob(data_dir . '/autoload/plug.vim'))
+            silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+            autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+        endif
+
+        call plug#begin()
+        Plug 'miguelmota/cairo.vim'
+        call plug#end()
+
         " Set true color mode
         if has('termguicolors')
             set termguicolors
